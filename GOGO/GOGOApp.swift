02c36f2ccd5d3fad617066b86b3733086cc2952f
Dinuka Shehan <Shehan.Shyaminda@gmail.com>
2024-10-29 22:41:42 +0530

    Initial Commit

---
+++ GOGO/GOGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOGOApp.swift
+//  GOGO
+//
+//  Created by Dinuka Shehan on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GOGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
